94cb9d9df7e28bfcae8f4563f997acd8e6de88f0
arluber <lqygo1234@qq.com>
2017-04-15 12:23:32 +0800

    layout调整

--- src/components/CoreLayout/index.js
+++ src/components/CoreLayout/index.js
@@ -1,6 +1,36 @@
-/**
- * Created by luqianyu on 2017/1/3.
- */
-import CoreLayout from './LayoutContainer'
+import React from 'react'
+import Nav from '../Nav'
+import './CoreLayout.css'
+import '../../styles/core.css'
+
+import { connect } from 'react-redux'
+import { login, signOut } from '../../actions/user'
+
+const Layout = React.createClass({
+  render () {
+    return (
+      <div className='container'>
+        <Nav {...this.props} />
+        <div className='core-layout__viewport'>
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+})
+
+function mapStateToProps (state) {
+  return {
+    isLogin: state.user.isLogin,
+    user: state.user.user
+  }
+}
+
+const mapDispatchToProps = {
+  login: (user) => login(user),
+  signOut: () => signOut()
+}
+
+const CoreLayout = connect(mapStateToProps, mapDispatchToProps)(Layout)
 
 export default CoreLayout
