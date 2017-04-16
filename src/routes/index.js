0e4054de3720753c7d211ab076392b71056eefb4
Crophone <1275589916@qq.com>
2017-04-16 18:24:35 +0800

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,46 +1,29 @@
 // We only need to import the modules necessary for initial render
 
-import CoreLayout from '../components/CoreLayout'
-import NotFind from '../components/404-not-find'
-import Home from '../containers/Home'
+import CoreLayout from '../layouts/CoreLayout'
 
-import {
-    counter,
-    about,
-    // demo1,
-    // demo2
-} from './demoCompoant'
-
-// import CounterRoute from './Counter'
-// import demoCompoant from '.demoCompoant'
-// import SignUpRoute from './SignUp'
-// import UserRoute from './User'
+import Home from './Home'
+import CounterRoute from './Counter'
+import AboutRoute from './About'
+import SignUpRoute from './SignUp'
+import UserRoute from './User'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
 
 export const createRoutes = (store) => ({
-    path: '/',
-    component: CoreLayout,
-    indexRoute: { component: Home },
-    childRoutes: [
-        //demoCompoant
-        //couter为原生的，Facebook官方脚手架的组件
-        counter(store),
-        //about为师兄的野鸡错误示范组件
-        about(store),
-        //   CounterRoute(store),
-        //   demoCompoant(store),
-        //   {
-        //     path        : '/signUp',
-        //     component   : SignUpRoute
-        //   },
-        //   UserRoute(store)
-        {
-            path: '*',
-            component: NotFind
-        },
-    ]
+  path        : '/',
+  component   : CoreLayout,
+  indexRoute  : Home,
+  childRoutes : [
+    CounterRoute(store),
+    AboutRoute(store),
+    {
+      path        : '/signUp',
+      component   : SignUpRoute
+    },
+    UserRoute(store)
+  ]
 })
 
 /*  Note: childRoutes can be chunked or otherwise loaded programmatically
